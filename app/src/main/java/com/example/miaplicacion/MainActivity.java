a94d2a7fc8c29df1ad715ea6ecf1bdfe2fd535ec
Yuliana López Palomino <i1820432@continental.edu.pe>
2019-11-17 23:01:44 -0500

    Mi segundo cambio

--- app/src/main/java/com/example/miaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miaplicacion/MainActivity.java
@@ -2,33 +2,55 @@ package com.example.miaplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnProducts;
+    private Button btnAboUts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnProducts =(Button) findViewById(R.id.btnEnter);
+        btnAboUts = (Button) findViewById(R.id.btnAboutUs);
+
+        btnProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,ProductListActivity.class);
+                startActivity(i);
+            }
+        });
+        btnAboUts.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+//Log.i(Settings.INFO,"AbaUts us");
+                Intent i = new Intent(MainActivity.this,AboutUsActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
     @Override
-    protected void onResume() {
+    protected void onResume(){
         super.onResume(); //este codigo se ejecuta antes de todos.
-        //En este punto la aplicacion ya cargo.
-
-        //This en un activity: es la esencia de una pantalla
-        //This es de tipo contexto
-
+//en este punto la aplicaion ya cargó.
+//this en un activity: es la esencia de un activity (de una pantalla)
+//this es de tipo contexto
         Toast.makeText(this,
                 "Aplicacion Iniciada",
                 Toast.LENGTH_LONG).show();
     }
     @Override
-    protected void onStop() {
+    protected void onStop(){
         super.onStop();
         Toast.makeText(this,
-                "Aplicacion stopeada",
+                "Aplicacion Stopeada",
                 Toast.LENGTH_LONG).show();
     }
 }
